de6b60ac6cc3e4e6129e109df0208959605b94c7
AyachiNene <1710082010@qq.com>
2017-09-27 23:22:36 +0800

    add .gitignore

---
+++ layout/header.php
@@ -0,0 +1,39 @@
+<!-- top-nav part -->
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	  	<div class="container">
+	    	<div class="navbar-header">
+	    		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		            <span class="sr-only">Toggle navigation</span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+	          	</button>
+	    		<!-- 咱社团logo也太丑了，我就不打算往上放了 -->
+	      		<a class="navbar-brand" href="index.php"><span>
+	      			哈工大腾讯俱乐部
+	      		</span></a>
+	    	</div>
+
+    		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      
+      			<ul class="nav navbar-nav navbar-right">
+					<li><a href="#" class="diyfont"></a></li>
+	        		<li><a href="#" class="diyfont"></a></li>
+	        		<li><a href="../login.php">登录</a></li>
+	        		<li><a href="#">注册</a></li>
+	      		</ul>
+
+	      		<form class="navbar-form navbar-right" action="search.php" method="get">
+	       			<div class="input-group">
+				      	<input type="text" name="search" class="form-control" placeholder="搜索">
+				      	<span class="input-group-btn">
+				        	<button class="btn btn-default diyfont" type="submit"></button>
+				      	</span>
+				    </div>
+	      		</form>
+	      		<ul class="nav navbar-nav navbar-right">
+					<li><a href="#" class="diyfont"></a></li>
+				</ul>
+	    	</div>
+	  	</div>
+	</nav>
